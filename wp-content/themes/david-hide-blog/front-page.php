ff52c79d863db5f4b4cb7f427602fee3f267cb77
David Hide <d.hide@live.co.uk>
2018-07-05 21:56:23 +0100

    Blog posts are now positioned above projects on front page

--- wp-content/themes/david-hide-blog/front-page.php
+++ wp-content/themes/david-hide-blog/front-page.php
@@ -7,6 +7,32 @@
 </section>
 <section>
   <div class="container u-margin-top-large">
+  <h2 class="heading--secondary">Recent Blog Posts</h2>
+    <a href="<?php echo esc_url(site_url('/blog')); ?>" class="link">View all posts &#x2192;</a>    
+    <div class="row">
+      <?php
+        $homepagePosts = new WP_Query(array(
+          'posts_per_page' => 2
+        ));
+
+        while($homepagePosts->have_posts()) {
+          $homepagePosts->the_post(); ?>
+          <div class="col-1-of-2">
+            <div class="u-margin-top-small">
+              <h2 class="post__title">
+                <a href="<?php the_permalink(); ?>" class="post__link">
+                  <?php echo get_the_post_thumbnail(get_the_id(), 'customThumbnail', array( 'class' => 'post__thumbnail' )); ?><br>
+                  <?php the_title(); ?>
+                </a>
+              </h2>
+              <h3 class="post__date"><?php the_date(); ?></h3>
+              <p><?php echo wp_trim_words(get_the_content(), 60); ?> <a href="<?php the_permalink(); ?>" class="link">Continue reading &raquo;</a></p>
+            </div>
+          </div>
+        <?php } wp_reset_postdata();
+      ?>
+    </div>
+    <hr class="u-hr">
     <h2 class="heading--secondary">Recent Projects</h2>
     <a href="<?php echo esc_url(site_url('/projects')); ?>" class="link">View all projects &#x2192;</a>    
     <div class="row">
@@ -28,32 +54,6 @@
               } else {
                 echo wp_trim_words(get_the_content(), 60); 
               }?> <a href="<?php the_permalink(); ?>" class="link">Continue reading &raquo;</a></p>
-            </div>
-          </div>
-        <?php } wp_reset_postdata();
-      ?>
-    </div>
-    <hr class="u-hr">
-    <h2 class="heading--secondary">Recent Blog Posts</h2>
-    <a href="<?php echo esc_url(site_url('/blog')); ?>" class="link">View all posts &#x2192;</a>    
-    <div class="row">
-      <?php
-        $homepagePosts = new WP_Query(array(
-          'posts_per_page' => 2
-        ));
-
-        while($homepagePosts->have_posts()) {
-          $homepagePosts->the_post(); ?>
-          <div class="col-1-of-2">
-            <div class="u-margin-top-small">
-              <h2 class="post__title">
-                <a href="<?php the_permalink(); ?>" class="post__link">
-                  <?php echo get_the_post_thumbnail(get_the_id(), 'customThumbnail', array( 'class' => 'post__thumbnail' )); ?><br>
-                  <?php the_title(); ?>
-                </a>
-              </h2>
-              <h3 class="post__date"><?php the_date(); ?></h3>
-              <p><?php echo wp_trim_words(get_the_content(), 60); ?> <a href="<?php the_permalink(); ?>" class="link">Continue reading &raquo;</a></p>
             </div>
           </div>
         <?php } wp_reset_postdata();
