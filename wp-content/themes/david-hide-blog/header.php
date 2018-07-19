866cfd20240ec4bf26c0875c3b6738f437c30296
David Hide <d.hide@live.co.uk>
2018-07-19 22:01:29 +0100

    Added favicon

--- wp-content/themes/david-hide-blog/header.php
+++ wp-content/themes/david-hide-blog/header.php
@@ -4,6 +4,7 @@
   <head>
       <meta charset="<?php bloginfo('charset'); ?>">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
 
       <?php wp_head(); ?>
   </head>
