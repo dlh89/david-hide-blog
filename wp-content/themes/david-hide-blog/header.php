cc8110f5f793e4057fce3df04fc4c715e95be2b2
David Hide <d.hide@live.co.uk>
2019-02-02 16:06:46 +0000

    Remove sign up and log in from nav bar

--- wp-content/themes/david-hide-blog/header.php
+++ wp-content/themes/david-hide-blog/header.php
@@ -19,12 +19,12 @@
             <li class="nav__item"><a href="<?php echo esc_url(site_url('/projects')); ?>" class="nav__link <?php if (is_archive('project') || is_singular('project')) echo 'nav__link--active' ?>">Projects</a></li>                                                                    
             <li class="nav__item"><a href="<?php echo esc_url(site_url('/about')); ?>" class="nav__link <?php if (is_page('about')) echo 'nav__link--active' ?>">About</a></li>
             <li class="nav__item"><a href="<?php echo esc_url(site_url('/contact')); ?>" class="nav__link <?php if (is_page('contact')) echo 'nav__link--active' ?>">Contact</a></li>
-            <?php if (is_user_logged_in()) { ?>
+            <!-- <?php if (is_user_logged_in()) { ?>
               <li class="nav__item"><a href="<?php echo wp_logout_url(); ?>" class="nav__link">Log Out</a></li>
             <?php } else { ?>
               <li class="nav__item"><a href="<?php echo wp_login_url(); ?>" class="nav__link <?php if (is_page('contact')) echo 'nav__link--active' ?>">Log In</a></li>
               <li class="nav__item"><a href="<?php echo wp_registration_url(); ?>" class="nav__link <?php if (is_page('contact')) echo 'nav__link--active' ?>">Sign Up</a></li>
-            <?php }?>            
+            <?php }?>             -->
             <li class="nav__item nav__item--search">
               <a href="<?php echo esc_url(site_url('/search')); ?>" class="nav__search-button" id="searchButton">
                 <svg class="icon icon--search"><use xlink:href="<?php echo get_template_directory_uri() ?>/dist/images/sprite.svg#icon-search"></use></svg>
