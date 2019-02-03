d53a550517a5df6e17c254b9acae82e9f03972ab
David Hide <d.hide@live.co.uk>
2019-02-03 12:08:00 +0000

    Change post and project titles to h1s

--- wp-content/themes/david-hide-blog/single-project.php
+++ wp-content/themes/david-hide-blog/single-project.php
@@ -3,7 +3,7 @@
   <div class="u-margin-top-medium">
     <div class="row">
       <div class="col-2-of-3">
-      <h2 class="post__title"><a href="<?php the_permalink(); ?>" class="post__link"><?php the_title(); ?></a></h2>
+      <h1 class="post__title"><a href="<?php the_permalink(); ?>" class="post__link"><?php the_title(); ?></a></h1>
         <a href="<?php the_field('project_link') ?>" target="_blank"><?php the_field('project_link') ?></a>
         <h3 class="post__date"><?php the_date(); ?></h3> <a href="<?php echo esc_url(site_url()); ?>" class="link">&#x2190; Back</a>
         <div class="project-panel row u-margin-top-medium">
