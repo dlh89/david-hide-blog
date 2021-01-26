b6993c592d342fb634bea9ade8cfc6c628fc4361
David Hide <d.hide@live.co.uk>
2021-01-26 20:57:16 +0000

    Bump stylesheet version

--- wp-content/themes/david-hide-blog/functions.php
+++ wp-content/themes/david-hide-blog/functions.php
@@ -11,7 +11,7 @@ function blog_files() {
     wp_enqueue_script( 'site_main_js', get_template_directory_uri() . '/dist/app.bundle.js' , null , microtime(), true );
   } else {
     // include a genuine version for production to enable caching
-    wp_enqueue_style( 'site_main_css', get_template_directory_uri() . '/dist/main.min.css' , null, 1.3);
+    wp_enqueue_style( 'site_main_css', get_template_directory_uri() . '/dist/main.min.css' , null, 1.4);
     wp_enqueue_script( 'site_main_js', get_template_directory_uri() . '/dist/app.bundle.js' , null , 1.1, true );
   }
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
